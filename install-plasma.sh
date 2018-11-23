f73e8e3367f0c06f81e239a57f965165211e1e9b
Adilson Schmitt Junior <adilsonschj@gmail.com>
2018-11-23 08:21:16 -0200

    Remove kwalletmanager

--- install-plasma.sh
+++ install-plasma.sh
@@ -30,6 +30,8 @@ PKG_DESKTOP+="ntp ufw bftpd "
 PKG_DESKTOP+="plasma partitionmanager "
 
 # kde-applications
+# NOTE: I'll leave kwalletmanager out for a time, it is a pain in the ass.
+# 
 PKG_DESKTOP+="dolphin dolphin-plugins dragon filelight "
 PKG_DESKTOP+="gwenview kamera kamoso kate "
 PKG_DESKTOP+="purpose kdegraphics-thumbnailers spectacle "
@@ -37,7 +39,7 @@ PKG_DESKTOP+="kde-cli-tools kcharselect ffmpegthumbs "
 PKG_DESKTOP+="kcolorchooser kcron kdenetwork-filesharing "
 PKG_DESKTOP+="kdesdk-thumbnailers kdf kdialog kfind kget "
 PKG_DESKTOP+="kgpg kleopatra kolourpaint kompare "
-PKG_DESKTOP+="ksystemlog kwalletmanager kwrite ocular "
+PKG_DESKTOP+="ksystemlog kwrite ocular "
 PKG_DESKTOP+="sweeper umbrello ark kcalc kdenlive khelpcenter "
 PKG_DESKTOP+="kio-extras kruler signon-kwallet-extension "
 PKG_DESKTOP+="kio-gdrive "
@@ -52,6 +54,7 @@ PKG_DESKTOP+="pavucontrol alsa-utils pulseaudio-zeroconf "
 PKG_DESKTOP+="libcanberra libcanberra-pulse "
 
 # Printer
+# This setup is not good yet... I can't configure printers as a normal user
 PKG_DESKTOP+="print-manager cups cups-pdf polkit cups-pk-helper "
 
 # (Fallback) Themes
@@ -129,22 +132,6 @@ sudo sh -c 'printf "export KWIN_TRIPLE_BUFFER=1" > /etc/profile.d/kwin.sh'
 
 # Reduce swappiness
 sudo sh -c 'printf "vm.swappiness=10" > /etc/sysctl.d/99-sysctl.conf'
-
-# create police to allow printers to be managed by normal users.
-# TODO: fix printer permission.
-# this polkit trick is not working.
-#_USERNAME=$(whoami)
-#sudo groupadd printereditor
-#sudo usermod -aG printereditor $_USERNAME
-#sudo cat >/etc/polkit-1/rules.d/49-allow-passwordless-printer-admin.rules
-#<<POLICEFILE
-#polkit.addRule(function(action, subject) {
-#    if (action.id == "org.opensuse.cupspkhelper.mechanism.all-edit" &&
-#        subject.isInGroup("printereditor")){
-#        return polkit.Result.YES;
-#    }
-#});
-#POLICEFILE
 
 # copy config files
 copy_files
