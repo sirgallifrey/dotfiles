cfc8ab1ffcbf3105ac13cbf2aa4295f087a6dc7f
Adilson Schmitt Junior <adilsonschj@gmail.com>
2019-04-24 14:09:34 -0300

    enhance dev env script

--- install-dev-env.sh
+++ install-dev-env.sh
@@ -2,11 +2,10 @@
 
 # apps
 PKG_DESKTOP+="chromium firefox-developer-edition pgadmin3 "
-PKG_DESKTOP+="docker docker-compose nodejs-lts-dubnium yarn "
+PKG_DESKTOP+="docker docker-compose yarn "
 PKG_DESKTOP+="dotnet-host dotnet-sdk dotnet-runtime aspnet-runtime "
-PKG_DESKTOP+="rustup go go-tools python "
-PKG_DESKTOP_AUR+="visual-studio-code-bin "
-
+PKG_DESKTOP+="rustup "
+PKG_DESKTOP_AUR+="visual-studio-code-bin nvm "
 
 # Install the listed software
 sudo pacman -S --needed --noconfirm ${PKG_DESKTOP}
@@ -20,5 +19,14 @@ if [[ -d ~/.vim/plugged/YouCompleteMe ]]; then
   python ./install.py --ts-completer --go-completer --rust-completer
 fi
 
-exec ./fix-npm-permissions.sh
+# use nvm stuff
+[[ -f /usr/share/nvm/init-nvm.sh ]] && source /usr/share/nvm/init-nvm.sh
+
+nvm install 10
+nvm use 10
+
+echo "Creating npm global dir for $(whoami)"
+mkdir -p ~/npm/bin
+npm config set prefix "$HOME/npm"
+
 
